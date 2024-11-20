d96aade5a8106398a33bab12ef783d1d237f460e
sergiogarino <sgarino22@gmail.com>
2024-11-19 22:23:30 -0600

    actualizo repo no recuerdo el ultimo cambio

--- src/Arduino/Arduino.ino
+++ src/Arduino/Arduino.ino
@@ -1,16 +1,12 @@
-#include <SoftwareSerial.h>
-
-SoftwareSerial espSerial(10, 11);  // RX, TX for ESP8266
-
 void setup() {
   Serial.begin(9600);    // Serial Monitor
-  espSerial.begin(9600); // Communication with ESP8266
+  Serial1.begin(9600);   // Communication with ESP8266 on Serial1 (TX1/RX1)
   randomSeed(analogRead(0));  // Seed the random number generator
 }
 
 void loop() {
-  int randomValue = random(0, 2);  // Generate 0 or 1
-  espSerial.println(randomValue);  // Send to ESP8266
+  int randomValue = random(0, 4);  // Generate a random value between 0 and 3
+  Serial1.println(randomValue);   // Send to ESP8266
   Serial.println("Sent to ESP: " + String(randomValue));  // Debugging
-  delay(2000);  // Send data every 2 seconds
+  delay(5000);  // Send data every 5 seconds
 }
